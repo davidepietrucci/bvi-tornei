52147f8ae235749fd149dbbdd3d5a243e6b5f462
davidepietrucci <tua-dadojo2001@gmail.com>
2026-04-29 16:14:24 +0200

    fix: allow google profile images

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,17 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
