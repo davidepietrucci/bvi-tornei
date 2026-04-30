a4667f030281af6cc9c9d6ed29a25f1b5133780d
davidepietrucci <dadojo2001@gmail.com>
2026-04-30 11:54:14 +0200

    Refactoring mobile-first e correzione errori di build

--- app/staff/tabellone/page.js
+++ app/staff/tabellone/page.js
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState, useEffect, Suspense } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import StaffHeader from "@/app/components/StaffHeader";
 
 function TabelloneContent() {
